cab435090c93a0ac0f71feb84ce7093c23a17ffe
DavidMendoza5 <cruz-azul99@hotmail.com>
2019-04-08 14:37:03 -0500

    Intento de creación de botó que se mueve

--- src/ejerciciohilosbotones/Ventana1.java
+++ src/ejerciciohilosbotones/Ventana1.java
@@ -4,16 +4,15 @@
  * and open the template in the editor.
  */
 package ejerciciohilosbotones;
-
+import ejerciciohilosbotones.Panel1;
 /**
  *
  * @author cruz-
  */
-public class Ventana1 extends javax.swing.JFrame {
+public class Ventana1 extends javax.swing.JFrame{
 
-    /**
-     * Creates new form Ventana1
-     */
+    Panel1 panel1 = new Panel1();
+    int x = getWidth();
     public Ventana1() {
         initComponents();
     }
@@ -27,21 +26,62 @@ public class Ventana1 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Panel1 = new javax.swing.JPanel();
+        Boton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        Boton1.setText("Contador");
+        Boton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Boton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout Panel1Layout = new javax.swing.GroupLayout(Panel1);
+        Panel1.setLayout(Panel1Layout);
+        Panel1Layout.setHorizontalGroup(
+            Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Boton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        Panel1Layout.setVerticalGroup(
+            Panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Panel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Boton1)
+                .addContainerGap(399, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Panel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(523, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(45, 45, 45)
+                .addComponent(Panel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void Boton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Boton1ActionPerformed
+
+        panel1.add(this);
+        panel1.inicio();
+        Boton1.setBounds(x, x, WIDTH, WIDTH);
+        panel1.updateUI();
+    }//GEN-LAST:event_Boton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +119,7 @@ public class Ventana1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Boton1;
+    private javax.swing.JPanel Panel1;
     // End of variables declaration//GEN-END:variables
 }
