0677f6c2ccd5e7f6851a671ac983d13016d07425
vincente yah <vicente.yah.dzul@icloud.com>
2019-04-08 13:33:43 -0500

    inittial commit

---
+++ src/ejerciciohilosbotones/EjercicioHilosBotones.java
@@ -0,0 +1,14 @@
+
+package ejerciciohilosbotones;
+
+
+public class EjercicioHilosBotones {
+
+    
+    public static void main(String[] args) {
+        Ventana1 v1 = new Ventana1();
+        v1.setVisible(true);
+        v1.setLocationRelativeTo(null);
+    }
+    
+}
